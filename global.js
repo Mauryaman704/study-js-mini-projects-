3ec2991971432b664ca97f0bea5b7858de44e73a
Mauryaman704 <am9198566563@gmail.com>
2025-06-20 22:57:16 +0530

    javascript notes 2

---
+++ global.js
@@ -0,0 +1,126 @@
+let a = 10 
+var b = 20
+const c = 40
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+function one(){
+    const username = "aman"
+
+    function two (){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+    // two()
+}
+
+// one()
+
+if(true){
+const username ="anchal"
+
+if(username==="anchal"){
+    const website ="youtube"
+    // console.log(username + website);
+    }
+    // console.log(website);
+    
+}
+
+// console.log(username);
+
+// +++++++++++++++++++++++++++intresting++++++++++++++++++++++++++++
+
+
+// function addOne(num){
+// return num +1
+
+// }
+
+// addone(4)
+
+// const addTwo =  function(num){
+//     return num + 2
+
+
+// addTwo(2)
+
+
+// +++++++++++++++arrow function+++++++++++++++++++++++++++++
+
+
+// const user = {
+//     username : "aman",
+//     price : 100,
+
+//     welcomeMessage :  function(){
+//         console.log(`${this.username} welcome to website`);
+//         console.log(this);
+        
+//     }
+// }
+
+// user.welcomeMessage()
+// user.username = "anchal"
+// user.welcomeMessage()
+
+// console.log(this);
+
+// function anchal (){
+//     let userName = "aman"
+//     console.log(this.userName);
+    
+// }
+
+// anchal()
+ 
+
+// const anchal = function(){
+//     let userName = "aman"
+//    console.log(this.userName);
+// }
+
+// anchal()
+
+
+//  const anchal = () => {
+//      let userName = "aman"
+//     console.log(this);
+//  }
+
+//  anchal()
+
+// const addTwo = (num1, num2) => {
+//             return num1 + num2
+// }
+
+// const addTwo = (num1, num2) => num1 + num2
+// const addTwo = (num1, num2) => (num1 + num2)
+// const addTwo = (num1, num2) => ({username:"aman"})
+
+
+// console.log(addTwo(2,3));
+
+
+// MyNewArray.forEach()
+
+
+//    IIFE(Immediately Invoked Function Expression)
+
+// (function chai(){
+//         console.log(`DB CONNECTED`)
+// }) ();
+
+
+// (function(){
+//     console.log(`DB CONNECTED TWO`)
+// }) ();
+
+// ((name) => {
+//     console.log(`DB CONNECTED TWO ${name}`)
+// }) ("aman")
+
